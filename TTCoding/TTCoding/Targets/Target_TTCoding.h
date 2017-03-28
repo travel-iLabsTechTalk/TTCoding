a4e0a03c8c3f5357a2a0a58de032855407922088
Travel.Chu <chuchuanming@gmail.com>
2017-03-28 20:43:12 +0800

    3

---
+++ TTCoding/TTCoding/Targets/Target_TTCoding.h
@@ -0,0 +1,14 @@
+//
+//  Target_TTCoding.h
+//  TTCoding
+//
+//  Created by Travel Chu on 3/28/17.
+//  Copyright © 2017 demo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Target_TTCoding : NSObject
+- (UIViewController *)Action_viewController;
+@end
---
+++ TTCoding/TTCoding/Targets/Target_TTCoding.m
@@ -0,0 +1,19 @@
+//
+//  Target_TTCoding.m
+//  TTCoding
+//
+//  Created by Travel Chu on 3/28/17.
+//  Copyright © 2017 demo. All rights reserved.
+//
+
+#import "Target_TTCoding.h"
+#import "TTCodingViewController.h"
+
+@implementation Target_TTCoding
+
+- (UIViewController *)Action_viewController {
+  TTCodingViewController *codingVC = [[UIStoryboard storyboardWithName:@"TTCoding" bundle:nil] instantiateViewControllerWithIdentifier:@"TTCodingViewController"];
+  return codingVC;
+}
+
+@end
